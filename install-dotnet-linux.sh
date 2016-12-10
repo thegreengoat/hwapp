7537d1840dd72da8cba931b04def48ac06188d00
Jason Allen <jason.thomas.allen@gmail.com>
2016-12-10 15:52:08 +0800

    enable install script

--- install-dotnet-linux.sh
+++ install-dotnet-linux.sh
@@ -1,4 +1,4 @@
-#sudo yum install libunwind libicu
-#curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=835019
-#sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
-#sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+sudo yum install libunwind libicu
+curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=835019
+sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
