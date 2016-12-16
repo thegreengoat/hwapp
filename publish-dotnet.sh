a9caf13b42c799f3f9364fc04b5591721f3e4355
Jason Allen <jason.thomas.allen@gmail.com>
2016-12-17 00:45:56 +0800

    Publish to test server

--- publish-dotnet.sh
+++ publish-dotnet.sh
@@ -1,1 +1,14 @@
+#publish
 dotnet publish
+
+#tar the output and scp to test server
+tar -cf hwapp.tar /var/lib/jenkins/workspace/hwapp/bin/Debug/netcoreapp1.1/publish/*
+scp -i ~/ecs-demo.pem hwapp.tar ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com:hwapp.tar
+
+#untar the binaries on the test server
+ssh -i ~/ecs-demo.pem ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com "tar -xf hwapp.tar"
+
+#run the app on the test server
+#ssh -i ~/ecs-demo.pem ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com "dotnet /var/lib/jenkins/workspace/hwapp/bin/Debug/netcoreapp1.1/publish/hwapp.dll"
+
+
