c4536f06f85dc9adc8664167ded621c3ace7db41
Jason Allen <jason.thomas.allen@gmail.com>
2016-12-18 19:24:30 +0800

    removed scp

--- publish-dotnet.sh
+++ publish-dotnet.sh
@@ -2,11 +2,11 @@
 dotnet publish
 
 #tar the output and scp to test server
-tar -cf hwapp.tar /var/lib/jenkins/workspace/hwapp/bin/Debug/netcoreapp1.1/publish/*
-scp -i /usr/share/pem/ecs-demo.pem hwapp.tar ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com:hwapp.tar
+#tar -cf hwapp.tar /var/lib/jenkins/workspace/hwapp/bin/Debug/netcoreapp1.1/publish/*
+#scp -i /usr/share/pem/ecs-demo.pem hwapp.tar ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com:hwapp.tar
 
 #untar the binaries on the test server
-ssh -i /usr/share/pem/ecs-demo.pem ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com "tar -xf hwapp.tar"
+#ssh -i /usr/share/pem/ecs-demo.pem ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com "tar -xf hwapp.tar"
 
 #run the app on the test server
 #ssh -i ~/ecs-demo.pem ec2-user@ec2-54-254-249-64.ap-southeast-1.compute.amazonaws.com "dotnet /var/lib/jenkins/workspace/hwapp/bin/Debug/netcoreapp1.1/publish/hwapp.dll"
