2790b3a82b9868df7c82e76c1232a4bbe4f3c10c
Jason Allen <jason.thomas.allen@gmail.com>
2017-01-27 01:13:10 +0800

    webhook

--- src/hello-world/HelloWorld.cs
+++ src/hello-world/HelloWorld.cs
@@ -12,7 +12,7 @@ namespace HelloWorld
         }
 
         public string getWelcomeMessage() {
-            var s = new StringBuilder("Hello from ASP.NET Core! Running on host ");
+            var s = new StringBuilder("Hello fella from ASP.NET Core! Running on host ");
             s.Append(System.Net.Dns.GetHostName());
             s.Append("\n\nHere is a random integer between 1 and 10 : ");
             s.AppendFormat("{0} ", _rg.getRandomInt(1, 10).ToString());
